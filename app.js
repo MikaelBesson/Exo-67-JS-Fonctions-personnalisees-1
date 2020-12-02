3cb0194e4cf47925f39484ac00b41bb8268a8f73
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-12-02 11:22:27 +0100

    exo 67

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+
+/**let texte1 ="Mon nouveaux texte" ;
+let texte2 = "Les fonction perso";
+let texte3 = "pas facile au debut";
+let texte4 = "mais on va rien lacher";
+let texte5 = "car tel est la voie";
+*/
+
+
+function monTexte(para, texte){
+    document.getElementById(para).innerHTML = texte;
+}
+
+
+monTexte("p1", "Mon nouveaux texte");
+
+monTexte("p2","les fonction perso");
+
+monTexte("p3", "pas facile au debut");
+
+monTexte("p4","mais on va rien lacher");
+
+monTexte("p5","car tel est la voie");
+
+
